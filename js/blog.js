1785bed9fff8fdce24e443fda15b72bee56c1757
notbadran <daffabadrann@gmail.com>
2024-10-18 23:40:20 +0700

    menyelesaikan task day 4

--- js/blog.js
+++ js/blog.js
@@ -4,23 +4,40 @@ function addBlog(event) {
   event.preventDefault();
 
   const inputBlogTitle = document.getElementById("input-blog-title").value;
+  const startDate = document.getElementById("start-date").value;
+  const endDate = document.getElementById("end-date").value;
   const inputBlogContent = document.getElementById("input-blog-content").value;
   const inputBlogImage = document.getElementById("input-blog-image").files;
+  //   cbSelected = [];
+
+  //   const checkbox = document.querySelectorAll("input[type=checkbox]");
+  //   for (let i = 0; i < checkbox.length; i++) {
+  //     if (document.getElementById(checkbox[i].value).checked == true) {
+  //       cbSelected.push(checkbox[i].value);
+  //     }
+  //   }
+
+  //   checkbox belum selesai
 
   const image = URL.createObjectURL(inputBlogImage[0]);
+  //   blob
 
   const blog = {
     title: inputBlogTitle,
     content: inputBlogContent,
+    startDate: startDate,
+    endDate: endDate,
+    // start sama end buat buat date
     createdAt: new Date(),
     image: image,
+    // image diatas diambil dari const image(blob) agar bisa di eksekusi saat memanggil array blogs bersama dengan object yg lainnya
   };
 
   blogs.unshift(blog);
   console.log(blogs);
   renderBlog();
 
-  //   renderBlog di taro disitu supaya nge menjalankan fungsu addBlog (menambah item)
+  //   renderBlog di taro disitu supaya menjalankan fungsi addBlog (menambah item) lebih dulu baru mengeksekusi renderblog
 }
 
 function renderBlog() {
@@ -41,21 +58,64 @@ function renderBlog() {
             </div>
             <h1>
               <a href="blog-detail.html" target="_blank"
-                >${blogs[index].title}</a
-              >
+                >${blogs[index].title}</a>
             </h1>
             <div class="detail-blog-content">
-                ${getFullTime(blogs[index].createdAt)} | daffa
+            ${getFullTime(blogs[index].createdAt)} | daffa
             </div>
             <p>
               ${blogs[index].content}
             </p>
-            <p>
-                ${getDistanceTime(blogs[index].createdAt)}
-            </p>
           </div>
         </div>`;
+
+    // $blogs index.image|title dan content merupakan src dari blog pada array blogs, tujuan mendefinisikan title | image | content, karena itu dinamakan seperti itu dan memanggil pada akhir statment dimaksudkan untuk memanggil hanya "itunya saja". jila .title, maka yg dipanggil hanya titlenya saja.
   }
 
   document.getElementById("contents").innerHTML = html;
 }
+function deleteBlog(index) {
+  blogs.splice(index, 1);
+  renderBlog();
+}
+// menghapus index tertentu dengan splice, parameter dilempar pada bagian delete blog dan di taruh pada parameter agar dapat ditangkap lagi
+
+function getFullTime(date) {
+  // perlu diingat penamaan parameter bebas
+  const months = [
+    "Jan",
+    "Feb",
+    "Marc",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const tanggal = date.getDate();
+  const bulan = months[date.getMonth()];
+  const tahun = date.getFullYear();
+
+  let jam = date.getHours();
+  let menit = date.getMinutes();
+
+  if (jam < 10) {
+    jam = "0" + jam;
+  }
+
+  if (menit < 10) {
+    menit = "0" + menit;
+  }
+
+  return `${tanggal} ${bulan} ${tahun} ${jam}:${menit} WIB`;
+}
+
+function esdate(startDate, endDate) {}
+// fungsi untuk start dan end date
+function getDistanceTime() {}
+// fungsi untuk durasi (pakai interval aja di akhir)
